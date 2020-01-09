9f5dc6c7e3b6dc0642b61a758e54552493d6e0f3
Soham Nagaokar <snagaokar6@gmail.com>
2020-01-09 16:33:22 -0500

    Added dict magic function

--- Processing.py
+++ Processing.py
@@ -7,14 +7,10 @@ class Item:
         self.isCheaper = None
     def setIsCheaper(self, isCheaper):
         self.isCheaper = isCheaper
-    def getName(self):
-        return self.name
     def getCost(self):
         return self.cost
-    def getSupplier(self):
-        return self.supplier
-    def getIsCheaper(self):
-        return self.isCheaper
+    def __dict__(self):
+        return {"name": self.name, "cost": self.cost, "supplier": self.supplier, "isCheaper": self.isCheaper}
 
 def find_min_relevant(options, item, supplier):
     for (name,cost) in options:
@@ -30,13 +26,13 @@ og_item_list = []
 target_item_list = []
 walmart_item_list = []
 for item in og_item_list:
-    walmart_options = get_walmart(item, "Walmart")
-    target_options = get_target(item, "Target")
+    walmart_options = get_walmart(item)
+    target_options = get_target(item)
     walmart_item = find_min_relevant(walmart_options, item)
     target_item = find_min_relevant(target_options, item)
     if walmart_item.getCost() > target_item.getCost():
         is_target_cheaper(True, target_item, walmart_item)
     else:
         is_target_cheaper(False, target_item, walmart_item)
-    target_item_list.append(target_item)
-    walmart_item_list.append(walmart_item)
+    target_item_list.append(target_item.__dict__)
+    walmart_item_list.append(walmart_item.__dict__)
