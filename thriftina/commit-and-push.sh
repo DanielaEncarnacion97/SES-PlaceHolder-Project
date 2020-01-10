85b72b4cffbad44b3b55caec17a77329ed3d504e
Su Lan Mair <sulan.mair@gmail.com>
2020-01-10 11:51:47 -0500

    removed extraneous folder

--- thriftina/commit-and-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build content and static files
-npm run build
-
-# Commit and push
-git add .
-git commit -m "Update"
-git push origin master 
--- thriftina/source/js/index.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ShoppingList = require('./components/ShoppingList');
-
-// format to pass info is name="info_to_pass"; ultimately not needed here but for future ref
-ReactDOM.render(<ShoppingList />, document.querySelector('[data-react-application]'));
